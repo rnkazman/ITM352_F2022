6068cad00313ea635ed308c56ad5ae88ae2eebf6
rnkazman <kazman@hawaii.edu>
2022-10-05 16:36:39 -1000

    Lab 10

--- Lab10/Ex4.js
+++ Lab10/Ex4.js
@@ -1,9 +1,28 @@
-function isNonNegInt (q) {
+// Function that takes a string as input and, depending on the value of returnErrors,
+// returns an array of errors or a boolean indicating: true if that string is a non-negative
+// integer and false otherwise
+function isNonNegativeInteger (queryString, returnErrors = false) {
     errors = []; // assume no errors at first
-    if(Number(q) != q) errors.push('Not a number!'); // Check if string is a number value
-    if(q < 0) errors.push('Negative value!'); // Check if it is non-negative
-    if(parseInt(q) != q) errors.push('Not an integer!'); // Check that it is an integer
+    if(Number(queryString) != queryString) errors.push('Not a number!'); // Check if string is a number value
+    if(queryString < 0) errors.push('Negative value!'); // Check if it is non-negative
+    if(parseInt(queryString) != queryString) errors.push('Not an integer!'); // Check that it is an integer
     
+    if (returnErrors) {
+        return errors;
+    } else if (errors.length == 0) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
+
+/*
+var attributes = "Daniel;21;21.5;-20.5";
+var pieces = attributes.split(";");
+
+for (i=0; i<pieces.length; i++) {
+    errorArray = isNonNegInt(pieces[i], true);
+    console.log(`i: ${i} ${errorArray.join(", ")}`);
 }
+*/
---
+++ Lab10/Test.js
@@ -0,0 +1,9 @@
+require("./Ex4.js");
+
+var attributes = "Daniel;21;21.5;-20.5";
+var pieces = attributes.split(";");
+
+for (i=0; i<pieces.length; i++) {
+    errorArray = isNonNegativeInteger(pieces[i], true);
+    console.log(`i: ${i} ${errorArray.join(", ")}`);
+}
