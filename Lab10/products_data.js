8235faa2b3198ee47e6f7160cfe2715caae1bc15
rnkazman <kazman@hawaii.edu>
2022-10-11 08:21:10 -1000

    Test_form

---
+++ Lab10/products_data.js
@@ -0,0 +1,29 @@
+console.log("Product variables loaded ... ");
+
+var products = [
+    product1 = {
+    name : "HTC",
+    price : 165.00,
+    image : "http://dport96.github.io/ITM352/morea/080.flow-control-II/HTC.jpg"
+    },
+    product2 = {
+    name : "Apple",
+    price : 285.00,
+    image : "http://dport96.github.io/ITM352/morea/080.flow-control-II/iphone-3gs.jpg"
+    },
+    product3 = {
+    name : "Nokia",
+    price : 135.00,
+    image : "http://dport96.github.io/ITM352/morea/080.flow-control-II/Nokia.jpg"
+    },
+    product4 = {
+    name : "Samsung",
+    price : 95.00,
+    image : "http://dport96.github.io/ITM352/morea/080.flow-control-II/Samsung.jpg"
+    },
+    product5 = {
+    name : "Blackberry",
+    price : 13.00,
+    image : "http://dport96.github.io/ITM352/morea/080.flow-control-II/Blackberry.jpg"
+    }
+    ];
