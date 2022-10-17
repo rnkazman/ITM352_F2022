8275e1fe9cbd3aa07eddb1c6ebfd436dad084668
rnkazman <kazman@hawaii.edu>
2022-10-17 11:56:37 -1000

    xxx

--- Test/BetterServer.js
+++ Test/BetterServer.js
@@ -1,15 +1,20 @@
+var path = require('path');
 var express = require('express');
+
 var app = express();
 
+var staticPath = path.join(__dirname, '/public');
+app.use(express.static(staticPath));
 
+app.get('/test', function(req, res, next) {
+    res.send('Request routed to test: ' + req.path);
+}) ;
 
 app.all('*', function (request, response, next) {
     response.send(request.method + ' to path= ' + request.path);
 });
 
-app.get('/test', function(req, res, next) {
-    res.send('Request routed to test: ' + req.path);
-}) ;
+
 
 app.listen(8080, () => console.log(`listening on port 8080`)); 
 // note the use of an anonymous function here to do a callback
