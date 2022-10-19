603276b6e27f982a996270eb7066b98fe9abd2d3
rnkazman <kazman@hawaii.edu>
2022-10-19 11:46:39 -1000

    asf

--- Test/BetterServer.js
+++ Test/BetterServer.js
@@ -6,18 +6,26 @@ var app = express();
 var staticPath = path.join(__dirname, '/public');
 
 app.use(express.static(staticPath));
+app.use(express.urlencoded({ extended: true }));
+
+app.all('*', function (request, response, next) {
+    console.log(request.method + ' to path= ' + request.path);
+    next();
+});
 
 app.get('/test', function(req, res, next) {
     res.send('Request routed to test: ' + req.path);
 }) ;
 
-app.all('*', function (request, response, next) {
-    response.send(request.method + ' to path= ' + request.path);
-});
-
 app.post("/process_form", function (request, response) {
+    console.log("Got process form");
+    response.type('txt') ;
     response.send(request.body); 
  });
+
+
+
+
  
 app.listen(8080, () => console.log(`listening on port 8080`)); 
 // note the use of an anonymous function here to do a callback
