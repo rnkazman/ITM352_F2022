0367ef9871a3c8d4d0246038ee1af0a3ea608f15
rnkazman <kazman@hawaii.edu>
2022-10-20 13:01:03 -1000

    Dealing with params

--- Test/BetterServer.js
+++ Test/BetterServer.js
@@ -13,19 +13,30 @@ app.all('*', function (request, response, next) {
     next();
 });
 
-app.get('/test', function(req, res, next) {
+var products = require(__dirname + '/product_data.json');
+
+app.get("/product_data.js", function (request, response, next) {
+    response.type('.js');
+    var products_str = `var products = ${JSON.stringify(products)};`;
+    response.send(products_str);
+});
+
+app.get('/test', function (req, res, next) {
     res.send('Request routed to test: ' + req.path);
-}) ;
+});
 
 app.post("/process_form", function (request, response) {
     console.log("Got process form");
-    response.type('txt') ;
-    response.send(request.body); 
- });
+    var q = request.body['quantity_textbox'];
+    let brand = products[0]['brand'];
+    let brand_price = products[0]['price'];
+
+    response.send(`<h2>Thank you for purchasing ${q} ${brand}. Your total is \$${q * brand_price}!</h2>`);
+});
 
 
 
 
- 
-app.listen(8080, () => console.log(`listening on port 8080`)); 
+
+app.listen(8080, () => console.log(`listening on port 8080`));
 // note the use of an anonymous function here to do a callback
