0b23a5f87cd0745fb39f6fcdba92320c96b3b17d
rnkazman <kazman@hawaii.edu>
2022-10-21 11:40:41 -1000

    Lab13

---
+++ Lab13/info_server_Ex2a.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+app.get('/test', function (request, response, next) {
+    console.log("Got a test path");
+    next();
+});
+
+app.all('*', function (request, response, next) {
+    response.send(request.method + ' to path ' + request.path);
+});
+app.listen(8080, () => console.log(`listening on port 8080`)); 
+
---
+++ Lab13/info_server_Ex2c.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/test', function (request, response, next) {
+    console.log("Got a test path");
+    next();
+});
+
+app.all('*', function (request, response, next) {
+    response.send(request.method + ' to path ' + request.path);
+});
+app.listen(8080, () => console.log(`listening on port 8080`)); 
+
