6146913775ef8ee6f74d9400d6eead525b3daa71
rnkazman <kazman@hawaii.edu>
2022-10-27 16:25:27 -1000

    New order page and receipt

--- Lab13/info_server_new.js
+++ Lab13/info_server_new.js
@@ -32,27 +32,39 @@ app.all('*', function (request, response, next) {
 });
 
 var products = require(__dirname + '/product_data.json');
-products.forEach( (prod,i) => {prod.total_sold = 0}); 
+products.forEach((prod, i) => { prod.total_sold = 0 });
 
 app.get("/product_data.js", function (request, response, next) {
-   response.type('.js');
-   var products_str = `var products = ${JSON.stringify(products)};`;
-   response.send(products_str);
+    response.type('.js');
+    var products_str = `var products = ${JSON.stringify(products)};`;
+    response.send(products_str);
 });
 
 app.post("/process_form", function (request, response) {
     // Process the form by redirecting to the receipt page
-    var q = request.body['text0'];
-    if (typeof q != 'undefined') {
-        if (isNonNegativeInteger(q)) {  // We have a valid quantity
-            products[0].total_sold += Number(q);
-            response.redirect('receipt.html?quantity=' + q);
+    let valid = true;
+    let ordered = "";
+
+    for (i = 0; i < products.length; i++) {
+        var name = "text" + i;
+        var q = request.body[name];
+        if (typeof q != 'undefined') {
+            if (isNonNegativeInteger(q)) {  // We have a valid quantity
+                products[i].total_sold += Number(q);
+                ordered += name + "=" + q + "&"; 
+            } else {
+                valid = false;
+            }
         } else {
-            response.redirect('order_page.html?error=Invalid%20Quantity&quantity_textbox=' + q);
+            valid = false;
         }
-    } else {
-        response.send("Enter a valid quantity!");
     }
+    if (!valid) {
+        response.redirect('order_page.new.html?error=Invalid%20Quantity');
+    } else {
+        response.redirect('receipt.new.html?' + ordered);
+    }
+
 });
 
 app.listen(8080, () => console.log(`listening on port 8080`));
