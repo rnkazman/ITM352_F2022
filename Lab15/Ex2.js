a070650e41382ffd076fd9f383e344ddd65786e6
rnkazman <kazman@hawaii.edu>
2022-11-18 13:32:53 -1000

    Updated Ex2

--- Lab15/Ex2.js
+++ Lab15/Ex2.js
@@ -4,7 +4,7 @@ var cookieParser = require('cookie-parser');
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 var session = require('express-session');
-app.use(session({secret: "MySecretKey", resave: true, saveUninitialized: true}));
+app.use(session({ secret: "MySecretKey", resave: true, saveUninitialized: true }));
 
 
 var fs = require('fs');
@@ -19,25 +19,33 @@ if (fs.existsSync(fname)) {
     console.log("Sorry file " + fname + " does not exist.");
 }
 
-app.get("/set_cookie", function(request, response) {
+app.get("/set_cookie", function (request, response) {
     let my_name = "Rick Kazman";
-    response.cookie("My new name", my_name, {maxAge: 30000}).send("Cookie sent");
+    response.cookie("My new name", my_name, { maxAge: 30000 }).send("Cookie sent");
 });
 
-app.get("/use_cookie", function(request, response) {
+app.get("/use_cookie", function (request, response) {
     let my_cookie = request.cookies["My new name"];
     response.send("Welcome to the use cookie page " + my_cookie);
 });
 
-app.get("/use_session", function(request, response) {
+app.get("/use_session", function (request, response) {
     response.send("Welcome!  Your session id is: " + request.session.id);
 });
 
 app.get("/login", function (request, response) {
     // Give a simple login form
+    if (typeof request.session.last_login != undefined) {
+        login_time = "Last login was " + request.session.last_login;
+    } else {
+        login_time = "First login";
+    }
+    my_cookie_name = request.cookies["username"];
+
     str = `
 <body>
 <form action="" method="POST">
+Login info: ${login_time} by ${my_cookie_name}<BR>
 <input type="text" name="username" size="40" placeholder="enter username" ><br />
 <input type="password" name="password" size="40" placeholder="enter password"><br />
 <input type="submit" value="Submit" id="submit">
@@ -45,7 +53,7 @@ app.get("/login", function (request, response) {
 </body>
     `;
     response.send(str);
- });
+});
 
 app.post("/login", function (request, response) {
     // Process login form POST and redirect to logged in page if ok, back to login page if not
@@ -54,15 +62,21 @@ app.post("/login", function (request, response) {
     let user_pass = POST["password"];
 
     console.log("User name=" + user_name + " password=" + user_pass);
-    
+
     if (users[user_name] != undefined) {
         if (users[user_name].password == user_pass) {
-            response.send("Good login for user " + user_name);
-        } else {
-            response.redirect("/login?error='Bad password'");
+            if (typeof request.session.last_login != undefined) {
+                var msg = `You last logged in: ${request.session.last_login}`;
+                var now = new Date();
+            } else {
+                var msg = '';
+                var now = "First visit";
+            }
         }
+        request.session.last_login = now;
+        response.cookie('username', user_name).send(`${msg} <BR>${user_name} logged in ${now}`);
     } else {
-        response.redirect("/login?error='No such user'");
+        response.send('No such user');
     }
 
 });
@@ -81,9 +95,9 @@ app.get("/register", function (request, response) {
 </body>
     `;
     response.send(str);
- });
+});
 
- app.post("/register", function (request, response) {
+app.post("/register", function (request, response) {
     // process a simple register form
     let POST = request.body;
     console.log(POST);
@@ -105,6 +119,6 @@ app.get("/register", function (request, response) {
     } else {
         response.send("User " + user_name + " already exists!");
     }
- });
+});
 
 app.listen(8080, () => console.log(`listening on port 8080`));
